3a5438fb2aabeb1210147b40835645f9ec96a7cd
soonann <soonann.tan@outlook.com>
2022-07-14 23:51:54 +0800

    added swagger to project

--- src/main/java/com/travelapp/backend/BackendApplication.java
+++ src/main/java/com/travelapp/backend/BackendApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableSwagger2
 @SpringBootApplication
 public class BackendApplication {
 
