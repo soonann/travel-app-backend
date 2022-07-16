2ff26af27f2dce3ed2801d898b66a9b4c0ee1305
soonann <soonann.tan@outlook.com>
2022-07-16 21:50:48 +0800

    added services to MockDataConfig

--- src/main/java/com/travelapp/backend/config/MockDataConfig.java
+++ src/main/java/com/travelapp/backend/config/MockDataConfig.java
@@ -1,14 +1,30 @@
 package com.travelapp.backend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.travelapp.backend.services.CustomTripService;
+import com.travelapp.backend.services.PremadeTripService;
+import com.travelapp.backend.services.UserService;
 
 
 @Configuration
 public class MockDataConfig {
     
-    // TODO: Add some sample data
-    // @Bean
-    //
+    
+    private UserService userService;
+    private PremadeTripService premadeTripService;
+    private CustomTripService customTripService;
+
+    @Autowired
+    public MockDataConfig(UserService userService, PremadeTripService premadeTripService, CustomTripService customTripService ){
+        this.userService = userService;
+        this.premadeTripService = premadeTripService;
+        this.customTripService = customTripService;
+    }
+
+    
+
 
 }
--- src/main/java/com/travelapp/backend/services/UserService.java
+++ src/main/java/com/travelapp/backend/services/UserService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     
-    private final UserRepository userRepository;
+    private UserRepository userRepository;
 
     @Autowired
     public UserService(UserRepository userRepository) {
