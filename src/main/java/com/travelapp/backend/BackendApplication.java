c7229b720d7b6b5e27b6decb3d53f4058d3e88c4
soonann <soonann.tan@outlook.com>
2022-07-21 00:30:02 +0800

    modified datastructures and removed token for local

--- src/main/java/com/travelapp/backend/BackendApplication.java
+++ src/main/java/com/travelapp/backend/BackendApplication.java
@@ -2,11 +2,11 @@ package com.travelapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+// import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+// @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableSwagger2
 @SpringBootApplication
 public class BackendApplication {
