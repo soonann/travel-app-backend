a65daef479ad4cf1a69d6dc0dba2d22ec0e7ec9e
soonann <soonann.dev@gmail.com>
2022-07-27 13:31:06 +0800

    changed production default response

--- src/main/java/com/travelapp/backend/controllers/HomeController.java
+++ src/main/java/com/travelapp/backend/controllers/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping
     public String homePage(){
-        return "HEAP Backend API Server asdasdasdsad";
+        return "Travel App Backend";
     }
 
 }
